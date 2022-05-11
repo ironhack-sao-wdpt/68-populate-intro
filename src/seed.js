5b20276c4c6a49db5c6cefdc90f2b4c3c2dc3311
rschpdr <pedro.resch@gmail.com>
2022-05-10 22:38:23 -0300

    added comments

--- src/seed.js
+++ src/seed.js
@@ -6,6 +6,7 @@ const OrderModel = require("./models/Order.model");
 
 module.exports = async () => {
   try {
+    // Limpando o banco antes de inserir novamente
     await UserModel.deleteMany();
     await ProductModel.deleteMany();
     await OrderModel.deleteMany();
@@ -55,9 +56,9 @@ module.exports = async () => {
 
     const myOrders = await OrderModel.findOne({ _id: order._id }).populate(
       "products.productId"
-    );
+    ); // o .populate é um método que recebe o nome do campo que deve ser populado (como string). Usando a referência criada anteriormente nos modelos, ele automaticamente busca e preenche os dados do documento linkado sem a necessidade de fazer outra consulta. Quando queremos popular um campo que é uma array ou objeto aninhado, podemos usar dot notation
 
-    console.log("Pedido populado => ", util.inspect(myOrders, { depth: null }));
+    console.log("Pedido populado => ", util.inspect(myOrders, { depth: null })); // o util.inspect é um método nativo do Node para receber mais informações sobre objetos no terminal. depth: null faz com que o terminal exiba todos os níveis de objetos aninhados e evite abreviações
   } catch (err) {
     console.error("seed error => ", err);
   }
